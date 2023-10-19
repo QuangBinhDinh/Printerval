7cdba2d3ce9022793321d8c54a6574bee9d2c775
QuangBinhDinh <g4.terminator@gmail.com>
2023-10-19 10:35:26 +0700

    change project name + bundle ID

--- android/app/src/main/java/com/printervalapp/MainApplication.java
+++ android/app/src/main/java/com/printervalapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.templatenew;
+package com.printervalapp;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/PrintervalTests/PrintervalTests.m
+++ ios/PrintervalTests/PrintervalTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TemplateNewTests : XCTestCase
+@interface PrintervalTests : XCTestCase
 
 @end
 
-@implementation TemplateNewTests
+@implementation PrintervalTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
