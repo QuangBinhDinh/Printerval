1ba8e55d57c7f813582afc80712bbe6f5595c3b7
QuangBinhDinh <g4.terminator@gmail.com>
2023-10-25 13:15:57 +0700

    setup cache time

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -4,3 +4,8 @@ export const RANDOM_IMAGE_URL = 'https://picsum.photos/400';
  * Số ngày hết hạn để fetch lại cây danh mục
  */
 export const CATEGORY_EXPIRE_DAY = 7;
+
+/**
+ * Thời gian (giây) lưu trữ cache của 1 fetch query
+ */
+export const CACHE_TIME_SECONDS = 45;
