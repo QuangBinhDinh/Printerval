df92ae8ce4b076e07192a95c99ce8559967e318b
QuangBinhDinh <g4.terminator@gmail.com>
2023-11-22 11:26:07 +0700

    fix address done

--- src/module/user/screen/AddressBook/index.tsx
+++ src/module/user/screen/AddressBook/index.tsx
@@ -1,11 +1,17 @@
 import HeaderScreen from '@components/HeaderScreen';
 import { createSelector } from '@reduxjs/toolkit';
 import React from 'react';
-import { FlatList, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import { RootState } from '@store/store';
 import { useSelector } from 'react-redux';
 import AddressCard from './AddressCard';
 import { ShippingAddress } from '@type/common';
+import { SCREEN_WIDTH } from '@util/index';
+import { lightColor } from '@styles/color';
+import { shadowTop } from '@styles/shadow';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import FancyButton from '@components/FancyButton';
+import { TextSemiBold } from '@components/text';
 
 const addressSelector = createSelector(
     [(state: RootState) => state.auth.addressBook, (state: RootState) => state.cart.defaultAddress],
@@ -21,11 +27,13 @@ const addressSelector = createSelector(
 );
 const AddressBook = () => {
     const data = useSelector(addressSelector);
+    const insets = useSafeAreaInsets();
 
     const renderItem = ({ item, index }: { item: ShippingAddress; index: number }) => (
         <AddressCard item={item} index={index} />
     );
 
+    const toCreateAddress = () => {};
     return (
         <View style={{ flex: 1, backgroundColor: 'white' }}>
             <HeaderScreen title="Address book" />
@@ -35,10 +43,44 @@ const AddressBook = () => {
                 style={{ flex: 1 }}
                 showsVerticalScrollIndicator={false}
                 contentContainerStyle={{ paddingHorizontal: 16, paddingTop: 32 }}
-                ListFooterComponent={<View style={{ height: 80 }} />}
+                ListFooterComponent={<View style={{ height: 100 }} />}
             />
+            <View
+                style={[
+                    styles.container,
+                    shadowTop,
+                    { height: 64 + insets.bottom / 2, paddingBottom: insets.bottom / 2 },
+                ]}
+            >
+                <FancyButton style={styles.button} backgroundColor={lightColor.secondary} onPress={toCreateAddress}>
+                    <TextSemiBold style={{ fontSize: 15, color: 'white' }}>Add new address</TextSemiBold>
+                </FancyButton>
+            </View>
         </View>
     );
 };
 
 export default AddressBook;
+
+const styles = StyleSheet.create({
+    container: {
+        height: 64,
+        width: SCREEN_WIDTH,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal: 18,
+        justifyContent: 'space-between',
+        position: 'absolute',
+        bottom: 0,
+    },
+    button: {
+        width: '100%',
+        height: 48,
+        borderRadius: 6,
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+        backgroundColor: lightColor.secondary,
+    },
+});
